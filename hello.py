c7d5ff0282adcfaaf6129ac579abc45eb1d3d2a1
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-04-19 22:27:32 +0200

    Made index file route

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request, redirect, url_for
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
@@ -10,20 +10,22 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 @app.route("/")
+@app.route("/home")
+@app.route("/Home")
 def hello():
-    return "Welcome to Python Flask App!"
+    return redirect(url_for('static', filename='index.html'))
 
-@app.route("/Register")
-def Authenticate():
-    username = request.args.get('Name')
-    password = request.args.get('Password')
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from Users where Name='" + username + "' and Password='" + password + "'")
-    data = cursor.fetchone()
-    if data is None:
-     return "Username or Password is wrong"
-    else:
-     return "Logged in successfully"
+#@app.route("/Register")
+#def Authenticate():
+#    username = request.args.get('Name')
+#    password = request.args.get('Password')
+#    cursor = mysql.connect().cursor()
+#    cursor.execute("SELECT * from Users where Name='" + username + "' and Password='" + password + "'")
+#    data = cursor.fetchone()
+#    if data is None:
+#     return "Username or Password is wrong"
+#    else:
+#     return "Logged in successfully"
 
 if __name__ == "__main__":
     app.run()
